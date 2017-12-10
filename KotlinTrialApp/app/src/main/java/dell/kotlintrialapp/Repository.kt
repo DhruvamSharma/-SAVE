6dbae76b24180fedde520770b9b7668b7e06f14e
DhruvamSharma <dhruvamssharma@gmail.com>
2017-12-10 18:20:33 +0530

    removed unnecessary folders

--- KotlinTrialApp/app/src/main/java/dell/kotlintrialapp/Repository.kt
+++
@@ -1,6 +0,0 @@
-package dell.kotlintrialapp
-
-/**
- * Created by dell on 08-09-2017.
- */
-class Repository(var repoName:String?,var authorName:String?,var haveIssues:Boolean?,var issueNumber: Int?,var stars: Int?)
